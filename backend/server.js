4bccb872d35147ce8cb0f2c653c29a425490b715
Berkay Gürcan <46679861+berkaygurcan@users.noreply.github.com>
2022-02-04 14:14:19 +0300

    düzenleme server.js

--- backend/server.js
+++ backend/server.js
@@ -22,19 +22,19 @@ app.use('/api/users', require('./routes/userRoutes')); //route oluşturma
 app.use('/api/tickets', require('./routes/ticketRoutes')); 
 
 // Serve Frontend
-if(process.env.NODE_ENV === 'production') {
-    //Set build folder as static
-    app.use(express.static(path.join(__dirname, '../frontend/build')));//static folder oluşturduk
-    app.get('*', (req, res) => res.sendFile(__dirname, '../','frontend',
-    'build','index.html'));
-} else {
-
-    //Route oluşturma
-    app.get('/',(req,res) => {
-    res.status(200).json({message: 'Welcome to the Support Desk API'}); //json döndürürüz
-})
-    
-}
+if (process.env.NODE_ENV === 'production') {
+    // Set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+  
+    // FIX: below code fixes app crashing on refresh in deployment
+    app.get('*', (_, res) => {
+      res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+    })
+  } else {
+    app.get('/', (req, res) => {
+      res.status(200).json({ message: 'Welcome to the Support Desk API' })
+    })
+  }
 
 app.use(errorHandler) //passing middleware
 
