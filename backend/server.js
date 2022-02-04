5b693011946341a68beea0999334ea6ede0421f2
Berkay Gürcan <46679861+berkaygurcan@users.noreply.github.com>
2022-02-04 14:27:01 +0300

    düzeltme

--- backend/server.js
+++ backend/server.js
@@ -28,8 +28,8 @@ if (process.env.NODE_ENV === 'production') {
   
     // FIX: below code fixes app crashing on refresh in deployment
     app.get('*', (_, res) => {
-      res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-    })
+        res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+      })
   } else {
     app.get('/', (req, res) => {
       res.status(200).json({ message: 'Welcome to the Support Desk API' })
